21d33aef38418074251febf56aa958567b9dd5aa
nicaa0695 <nicaa0695@gmail.com>
2019-11-08 14:41:55 -0700

    added getWorkouts function and listenFor Click function to workouts.js

--- app/assets/javascripts/workouts.js
+++ app/assets/javascripts/workouts.js
@@ -1,5 +1,28 @@
 $(function() {
 	console.log('workouts.js is loaded ...')
-	
+	listenForClick()
+	// listenForNewWorkoutFormClick()
 });
-const abc = 12
+
+function listenForClick() {
+	$('button#workouts-data').on('click', function (event) {
+		event.preventDefault()
+		getWorkouts()
+	})
+}
+
+function getWorkouts() {
+	$.ajax({
+		url: 'http://localhost:3000/workouts',
+		method: 'get',
+		dataType: 'json',
+		success: function (data) {
+			console.log("the data is: ", data)
+			data.map(post => {
+				const newWorkout = new Workout(workout)
+				const newWorkoutHtml = newPost.postHTML()
+				document.getElementById('ajax-workouts').innerHTML += newWorkoutHtml
+			})
+		}
+	})
+}
